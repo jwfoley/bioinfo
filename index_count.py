95fba8d04a7e06f90bab683bf96fe7270d2246cc
Joe Foley <jwfoley@stanford.edu>
2017-12-07 11:54:46 -0800

    use sample sheet

--- index_count.py
+++ index_count.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env pypy
 
-# given a list of known index sequences and a FASTQ, count the number of times each index seems to be observed, with mismatch tolerance
+# count the number of times each index sequence is be observed
+# if a table of known sequences is provided, count hits to each one, with mismatch tolerance
+# if a sample sheet is also provided, label known sequences with sample IDs
 # FASTQ sequences are truncated to the length of the known index sequences
 # this will double-count reads that match more than one index!
-# usage: collision_count.py index_table.tsv < index_reads.fastq
+
+# usage: collision_count.py [index_table.tsv] [SampleSheet.csv] < index_reads.fastq
+
 # index_table.tsv should be tab-delimited, two columns: first is name, second is sequence
-# dumps frequency of known indices to STDOUT and most common reads (known or not) to STDERR
+# dumps most common read counts to STDOUR and counts of known indices to STDERR
+
 
 from __future__ import print_function
 import sys, collections, distance
@@ -13,13 +18,34 @@ import sys, collections, distance
 ALLOWED_MISMATCHES = 1 # bcl2fastq default
 N_MOST_COMMON = 20 # number of most common reads to report
 
+read_counts = collections.Counter()
 index_table = collections.OrderedDict()
 index_counts = collections.Counter()
-read_counts = collections.Counter()
+sample_table = collections.defaultdict(str)
 
-for line in open(sys.argv[1]):
-	name, sequence = line.rstrip().split('\t')
-	index_table[name] = sequence
+
+if len(sys.argv) >= 2:
+	for line in open(sys.argv[1]):
+		name, sequence = line.rstrip().split('\t')
+		index_table[name] = sequence
+
+if len(sys.argv) >= 3:
+	in_data = False
+	index_column = None
+	for line in open(sys.argv[2]):
+		if in_data:
+			parsed_line = line.rstrip().split(',')
+			sample_table[parsed_line[index_column]] = parsed_line[0]
+		elif line.startswith('Sample_ID'):
+			parsed_line = line.rstrip().split(',')
+			column = 0
+			for field in parsed_line:
+				if field == 'index':
+					index_column = column
+					break
+				column += 1
+			in_data = True
+
 
 line_n = 0
 for line in sys.stdin:
@@ -31,9 +57,10 @@ for line in sys.stdin:
 		for name, known_sequence in index_table.items():
 			index_counts[name] += (distance.hamming(known_sequence, index_read[:len(known_sequence)]) <= ALLOWED_MISMATCHES)
 
-for name, sequence in index_table.items():
-	print('%s\t%s\t%i' % (name, sequence, index_counts[name]))
 
 for read_freq in read_counts.most_common(N_MOST_COMMON):
-	print('%s\t%i' % read_freq, file = sys.stderr)
+	print('%s\t%i' % read_freq)
+
+for index_name, sequence in index_table.items():
+	print('%s\t%s\t%s\t%i' % (sample_table[sequence], index_name, sequence, index_counts[index_name]), file = sys.stderr)	
 
