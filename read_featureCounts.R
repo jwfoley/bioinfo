5ecdaec9fe59e2e993182cf58d2f1addd1f20913
Joseph W. Foley <jwfoley@nus.edu.sg>
2023-08-03 09:58:55 +0800

    warn of duplicate colnames, simplification optional

--- read_featureCounts.R
+++ read_featureCounts.R
@@ -1,8 +1,8 @@
 # easily read counts from Subread featureCounts output
 # omit gene position info, read only a matrix of integer counts
-# convert BAM/CRAM filenames to basenames
+# optionally convert BAM/CRAM file paths to basenames, but this can create duplicates
 
-read.featureCounts <- function(file) {
+read.featureCounts <- function(file, simplify = TRUE) {
 	raw <- read.table(file,
 		header = T,
 		row.names = 1,
@@ -12,7 +12,8 @@ read.featureCounts <- function(file) {
 	)
 	stopifnot("unrecognized column headers" = all(colnames(raw)[1:5] == c("Chr", "Start", "End", "Strand", "Length")))
 	result <- as.matrix(raw[,6:ncol(raw)])
-	colnames(result) <- sub("\\.bam$|\\.cram$", "", basename(colnames(result)))
+	if (simplify) colnames(result) <- sub("\\.bam$|\\.cram$", "", basename(colnames(result)))
+	if (any(duplicated(colnames(result)))) warning("duplicate colnames")
 	result
 } 
 
