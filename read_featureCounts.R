eec1e902b7416c8826d0fdd79d5952c1e8ea5d81
Joseph W. Foley <joe@jwfoley.com>
2023-11-15 17:06:24 +0800

    more safety checks

--- read_featureCounts.R
+++ read_featureCounts.R
@@ -1,19 +1,26 @@
 # easily read counts from Subread featureCounts output
 # omit gene position info, read only a matrix of integer counts
-# optionally convert BAM/CRAM file paths to basenames, but this can create duplicates
+# optionally remove directory paths, but this can create duplicates
 
-read.featureCounts <- function(file, simplify = TRUE) {
+read.featureCounts <- function(file, remove.dir = TRUE) {
 	raw <- read.table(file,
 		header = T,
-		row.names = 1,
 		check.names = F,
 		stringsAsFactors = F,
 		sep = "\t"
 	)
-	stopifnot("unrecognized column headers" = all(colnames(raw)[1:5] == c("Chr", "Start", "End", "Strand", "Length")))
-	result <- as.matrix(raw[,6:ncol(raw)])
-	if (simplify) colnames(result) <- sub("\\.bam$|\\.cram$", "", basename(colnames(result)))
-	if (any(duplicated(colnames(result)))) warning("duplicate colnames")
-	result
+	stopifnot("unrecognized featureCounts format" = identical(c("Geneid", "Chr", "Start", "End", "Strand", "Length"), colnames(raw)[1:6]))
+	counts <- as.matrix(raw[,7:ncol(raw)])
+	
+	duplicate.genes <- unique(raw$Geneid[duplicated(raw$Geneid)])
+	if (length(duplicate.genes) > 0) warning(paste(c("duplicate gene IDs:", duplicate.genes), collapse = " "))
+	rownames(counts) <- raw$Geneid
+	
+	colnames(counts) <- sub("\\.bam$|\\.cram$", "", colnames(counts))
+	if (remove.dir) colnames(counts) <- basename(colnames(counts))
+	duplicate.samples <- unique(colnames(counts)[duplicated(colnames(counts))])
+if (length(duplicate.samples) > 0) warning(paste(c("duplicate sample names:", duplicate.samples), collapse = " "))
+	
+	return(counts)
 } 
 
